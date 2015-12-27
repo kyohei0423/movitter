c3745dde35ede60d13faa3198c49bb25d43e0989
Kyohei Seo <kyo199333@gmail.com>
2015-12-27 21:01:07 +0900

    add some files for new post

---
+++ Movitter/NewPostView.swift
@@ -0,0 +1,21 @@
+//
+//  NewPostView.swift
+//  Movitter
+//
+//  Created by Seo Kyohei on 2015/12/27.
+//  Copyright © 2015年 Kyohei Seo. All rights reserved.
+//
+
+import UIKit
+
+class NewPostView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
