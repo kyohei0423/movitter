e9b6cb51fd21533ba49e06011ce3a9f076610bbf
Kyohei Seo <kyo199333@gmail.com>
2015-12-27 19:23:18 +0900

    initial commit

---
+++ Movitter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Movitter
+//
+//  Created by Seo Kyohei on 2015/12/27.
+//  Copyright © 2015年 Kyohei Seo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
