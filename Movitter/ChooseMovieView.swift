6aa36fb861476357db2843b1d2699ee49ace5180
Kyohei Seo <kyo199333@gmail.com>
2016-03-23 17:03:39 +0900

    クラス名を変更した viewControllerのviewを作成したviewに変更した

--- Movitter/ChooseMovieView.swift
+++ Movitter/ChooseMovieView.swift
@@ -8,19 +8,34 @@
 
 import UIKit
 
-class ChoosePhotoView: UIView {
+class ChooseMovieView: UIView {
 
     @IBOutlet weak var previewView: UIView!
     @IBOutlet weak var movieCollectionView: UICollectionView!
     
     override func layoutSubviews() {
         super.layoutSubviews()
+        
         previewView.frame.origin = CGPointZero
         previewView.frame.size = CGSize(width: self.frame.width, height: self.frame.width)
+        let nib = UINib(nibName: "MovieCollectionViewCell", bundle: nil)
         
+        movieCollectionView.registerNib(nib, forCellWithReuseIdentifier: "MovieCollectionViewCell")
         movieCollectionView.frame.origin = CGPoint(x: 0, y: self.frame.height)
         movieCollectionView.frame.size = CGSize(width: self.frame.width, height: self.frame.height - self.frame.width)
+        print(movieCollectionView.frame)
+        setCollectionViewFlowLayout()
     }
     
+    
+    
+    private func setCollectionViewFlowLayout() {
+        let flowLayout = UICollectionViewFlowLayout()
+        let itemLength = frame.width / 3
+        flowLayout.itemSize = CGSize(width: itemLength, height: itemLength)
+        flowLayout.minimumLineSpacing = 0
+        flowLayout.minimumInteritemSpacing = 0
+        movieCollectionView.collectionViewLayout = flowLayout
+    }
     
 }
--- Movitter/ChooseMovieViewController.swift
+++ Movitter/ChooseMovieViewController.swift
@@ -8,16 +8,33 @@
 
 import UIKit
 
-class ChoosePhotoViewController: UIViewController {
+class ChooseMovieViewController: UIViewController, UICollectionViewDelegate {
+    let movieManager = MovieManager.sharedMovieManager
 
+    override func loadView() {
+        super.loadView()
+        let nib = UINib(nibName: "ChooseMovieView", bundle: nil)
+        view = nib.instantiateWithOwner(nil, options: nil).first as! UIView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let chooseMovieView = view as! ChooseMovieView
+        chooseMovieView.movieCollectionView.delegate = self
+        chooseMovieView.movieCollectionView.dataSource = movieManager
     }
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Back", style: .Plain, target: self, action: "tapBackButton")
         
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        let chooseMovieView = view as! ChooseMovieView
+        movieManager.getMoviesFromCameraroll()
+        chooseMovieView.movieCollectionView.reloadData()
     }
 
     override func didReceiveMemoryWarning() {
